1493aae988957b13bc92239e7872641a8fbf3dc4
Ilham R.A <ilhamrahmat652@gmail.com>
2022-06-14 02:46:58 +0700

    Contoh

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'namalibrarynya/belajar-php-composer-hello',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'namalibrarynya/belajar-php-composer-hello' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
